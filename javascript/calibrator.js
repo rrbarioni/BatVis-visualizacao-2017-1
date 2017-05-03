022717d441e95841479e0337b91615e9ae6b217f
rrbarioni <rrb@cin.ufpe.br>
2017-05-03 08:30:21 -0300

    [calibrator] cells and lines working (better change lines' color)

--- javascript/calibrator.js
+++ javascript/calibrator.js
@@ -13,25 +13,30 @@ class Calibrator {
 
 		this.calibratorLinesContainer = this.container.append("g")
 			.attr("class", "calibratorLines");
-		this.calibratorLines;
+		this.calibratorLinesValues, this.calibratorLines;
 		this.setCalibratorLines();
 
-		this.calibratorGridContainer = this.container.append("g")
-			.attr("class", "calibratorGrid");
-		this.calibratorGrid;
-		this.setCalibratorGrid();
+		this.calibratorCellsContainer = this.container.append("g")
+			.attr("class", "calibratorCells");
+		this.calibratorCellsValues, this.calibratorCells;
+		this.setCalibratorCells();
 	}
 
-	setCalibratorLines() {
-		this.calibratorLines = [
+	updateCalibratorLinesValues() {
+		this.calibratorLinesValues = [
 			{ "id": "leftLine",	  "x": this.width * 1/3, "y": 0,                 "width": this.lineThickness, "height": this.height        },
 			{ "id": "rightLine",  "x": this.width * 2/3, "y": 0,                 "width": this.lineThickness, "height": this.height        },
 			{ "id": "topLine",	  "x": 0,                "y": this.height * 1/3, "width": this.width,         "height": this.lineThickness },
 			{ "id": "bottomLine", "x": 0,                "y": this.height * 2/3, "width": this.width,         "height": this.lineThickness }
 		];
+	}
 
-		this.calibratorLinesContainer.selectAll(".calibratorLine")
-			.data(this.calibratorLines)
+	setCalibratorLines() {
+		this.updateCalibratorLinesValues();
+
+		this.calibratorLines = this.calibratorLinesContainer
+			.selectAll(".calibratorLine")
+			.data(this.calibratorLinesValues)
 			.enter()
 			.append("rect")
 			.attr("class", "calibratorLine")
@@ -45,52 +50,72 @@ class Calibrator {
 
 	dragCalibratorLine(d) {
 		switch(d.id) {
-			case "leftLine":   this.calibratorLinesContainer.select("#leftLine").attr  ("x", d.x = Math.max(0, Math.min(this.calibratorLines[this.getLineIndexById("rightLine")].x,d3.event.x))); break;
-			case "rightLine":  this.calibratorLinesContainer.select("#rightLine").attr ("x", d.x = Math.max(this.calibratorLines[this.getLineIndexById("leftLine")].x, Math.min(this.width,d3.event.x))); break;
-			case "topLine":    this.calibratorLinesContainer.select("#topLine").attr   ("y", d.y = Math.max(0, Math.min(this.calibratorLines[this.getLineIndexById("bottomLine")].y,d3.event.y))); break;
-			case "bottomLine": this.calibratorLinesContainer.select("#bottomLine").attr("y", d.y = Math.max(this.calibratorLines[this.getLineIndexById("topLine")].y, Math.min(this.height,d3.event.y))); break;
+			case "leftLine":   { this.calibratorLinesContainer.select("#leftLine").attr  ("x", d.x = Math.max(0, Math.min(this.calibratorLinesValues[this.getLineIndexById("rightLine")].x,d3.event.x)));         this.updateCalibratorCells(); break; }
+			case "rightLine":  { this.calibratorLinesContainer.select("#rightLine").attr ("x", d.x = Math.max(this.calibratorLinesValues[this.getLineIndexById("leftLine")].x, Math.min(this.width,d3.event.x))); this.updateCalibratorCells(); break; }
+			case "topLine":    { this.calibratorLinesContainer.select("#topLine").attr   ("y", d.y = Math.max(0, Math.min(this.calibratorLinesValues[this.getLineIndexById("bottomLine")].y,d3.event.y)));        this.updateCalibratorCells(); break; }
+			case "bottomLine": { this.calibratorLinesContainer.select("#bottomLine").attr("y", d.y = Math.max(this.calibratorLinesValues[this.getLineIndexById("topLine")].y, Math.min(this.height,d3.event.y))); this.updateCalibratorCells(); break; }
 		}
 	}
 
-	setCalibratorGrid() {
-		var leftLineX =   this.calibratorLines[this.getLineIndexById("leftLine")].x;
-		var rightLineX =  this.calibratorLines[this.getLineIndexById("rightLine")].x;
-		var topLineY =    this.calibratorLines[this.getLineIndexById("topLine")].y;
-		var bottomLineY = this.calibratorLines[this.getLineIndexById("bottomLine")].y;
+	updateCalibratorCellsValues() {
+		var leftLineX =   this.calibratorLinesValues[this.getLineIndexById("leftLine")].x;
+		var rightLineX =  this.calibratorLinesValues[this.getLineIndexById("rightLine")].x;
+		var topLineY =    this.calibratorLinesValues[this.getLineIndexById("topLine")].y;
+		var bottomLineY = this.calibratorLinesValues[this.getLineIndexById("bottomLine")].y;
 
-		this.calibratorGrid = [
-			{ "id": "cell1", "x": 0,                               "y": 0,                                "width": leftLineX,                                      "height": topLineY,                                         "status": "neutral" },
-			{ "id": "cell2", "x": leftLineX + this.lineThickness,  "y": 0,                                "width": rightLineX - (leftLineX + this.lineThickness),  "height": topLineY,                                         "status": "neutral" },
-			{ "id": "cell3", "x": rightLineX + this.lineThickness, "y": 0,                                "width": this.width - (rightLineX + this.lineThickness), "height": topLineY,                                         "status": "neutral" },
-			{ "id": "cell4", "x": 0,                               "y": topLineY + this.lineThickness,    "width": leftLineX,                                      "height": bottomLineY - (topLineY + this.lineThickness),    "status": "neutral" },
-			{ "id": "cell5", "x": leftLineX + this.lineThickness,  "y": topLineY + this.lineThickness,    "width": rightLineX - (leftLineX + this.lineThickness),  "height": bottomLineY - (topLineY + this.lineThickness),    "status": "neutral" },
-			{ "id": "cell6", "x": rightLineX + this.lineThickness, "y": topLineY + this.lineThickness,    "width": this.width - (rightLineX + this.lineThickness), "height": bottomLineY - (topLineY + this.lineThickness),    "status": "neutral" },
-			{ "id": "cell7", "x": 0,                               "y": bottomLineY + this.lineThickness, "width": leftLineX,                                      "height": this.height - (bottomLineY + this.lineThickness), "status": "neutral" },
-			{ "id": "cell8", "x": leftLineX + this.lineThickness,  "y": bottomLineY + this.lineThickness, "width": rightLineX - (leftLineX + this.lineThickness),  "height": this.height - (bottomLineY + this.lineThickness), "status": "neutral" },
-			{ "id": "cell9", "x": rightLineX + this.lineThickness, "y": bottomLineY + this.lineThickness, "width": this.width - (rightLineX + this.lineThickness), "height": this.height - (bottomLineY + this.lineThickness), "status": "neutral" }
+		this.calibratorCellsValues = [
+			{ "id": "cell1", "x": 0,                               "y": 0,                                "width": Math.max(0, leftLineX),                                      "height": Math.max(0, topLineY),                                         "status": "neutral" },
+			{ "id": "cell2", "x": leftLineX + this.lineThickness,  "y": 0,                                "width": Math.max(0, rightLineX - (leftLineX + this.lineThickness)),  "height": Math.max(0, topLineY),                                         "status": "neutral" },
+			{ "id": "cell3", "x": rightLineX + this.lineThickness, "y": 0,                                "width": Math.max(0, this.width - (rightLineX + this.lineThickness)), "height": Math.max(0, topLineY),                                         "status": "neutral" },
+			{ "id": "cell4", "x": 0,                               "y": topLineY + this.lineThickness,    "width": Math.max(0, leftLineX),                                      "height": Math.max(0, bottomLineY - (topLineY + this.lineThickness)),    "status": "neutral" },
+			{ "id": "cell5", "x": leftLineX + this.lineThickness,  "y": topLineY + this.lineThickness,    "width": Math.max(0, rightLineX - (leftLineX + this.lineThickness)),  "height": Math.max(0, bottomLineY - (topLineY + this.lineThickness)),    "status": "neutral" },
+			{ "id": "cell6", "x": rightLineX + this.lineThickness, "y": topLineY + this.lineThickness,    "width": Math.max(0, this.width - (rightLineX + this.lineThickness)), "height": Math.max(0, bottomLineY - (topLineY + this.lineThickness)),    "status": "neutral" },
+			{ "id": "cell7", "x": 0,                               "y": bottomLineY + this.lineThickness, "width": Math.max(0, leftLineX),                                      "height": Math.max(0, this.height - (bottomLineY + this.lineThickness)), "status": "neutral" },
+			{ "id": "cell8", "x": leftLineX + this.lineThickness,  "y": bottomLineY + this.lineThickness, "width": Math.max(0, rightLineX - (leftLineX + this.lineThickness)),  "height": Math.max(0, this.height - (bottomLineY + this.lineThickness)), "status": "neutral" },
+			{ "id": "cell9", "x": rightLineX + this.lineThickness, "y": bottomLineY + this.lineThickness, "width": Math.max(0, this.width - (rightLineX + this.lineThickness)), "height": Math.max(0, this.height - (bottomLineY + this.lineThickness)), "status": "neutral" }
 		];
+	}
 
-		console.log(this.calibratorGrid[0].x + ", " + 
-					this.calibratorGrid[0].y + ", " + 
-					this.calibratorGrid[0].width + ", " + 
-					this.calibratorGrid[0].height);
+	updateCalibratorCells() {
+		this.updateCalibratorCellsValues();
 
-		this.calibratorGridContainer.selectAll(".calibratorCell")
-			.data(this.calibratorGrid)
+		this.calibratorCells
+			.data(this.calibratorCellsValues)
+			.attr("x",      function(d) { return d.x;      })
+			.attr("y",      function(d) { return d.y;      })
+			.attr("width",  function(d) { return d.width;  })
+			.attr("height", function(d) { return d.height; });
+	}
+
+	setCalibratorCells() {
+		this.updateCalibratorCellsValues();
+
+		this.calibratorCells = this.calibratorCellsContainer
+			.selectAll(".calibratorCell")
+			.data(this.calibratorCellsValues)
 			.enter()
 			.append("rect")
-			.attr("class", "calibratorGrid")
+			.attr("class", "calibratorCell")
 			.attr("id",     function(d) { return d.id;     })
 			.attr("x",      function(d) { return d.x;      })
 			.attr("y",      function(d) { return d.y;      })
 			.attr("width",  function(d) { return d.width;  })
 			.attr("height", function(d) { return d.height; })
-			.attr("status", function(d) { return d.status; });
+			.attr("status", function(d) { return d.status; })
+			.on("click", this.changeCellStatus.bind(this));
+	}
+
+	changeCellStatus(d) {
+		switch(d.status) {
+			case "neutral":  this.calibratorCellsContainer.select("#" + d.id).attr("status", d.status = "entrance"); break;
+			case "entrance": this.calibratorCellsContainer.select("#" + d.id).attr("status", d.status = "exit");     break;
+			case "exit":     this.calibratorCellsContainer.select("#" + d.id).attr("status", d.status = "neutral");  break;
+		}	
 	}
 
 	getLineIndexById(id) {
-		for(var i = 0; i < this.calibratorLines.length; i++) {
-			if (id == this.calibratorLines[i].id) {
+		for(var i = 0; i < this.calibratorLinesValues.length; i++) {
+			if (id == this.calibratorLinesValues[i].id) {
 				return i;
 			}
 		}
