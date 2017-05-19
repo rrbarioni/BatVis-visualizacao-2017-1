8e6ebde8b63ca6579bd8dada65d59569dda48a49
duduRodrigues <ehmr@cin.ufpe.br>
2017-05-19 14:46:06 -0300

    Refactor the 3D viewer and put it on main

--- batPopulation/main.js
+++ batPopulation/main.js
@@ -14,11 +14,15 @@ var flightsHistogramHeight = 550;
 var averageFlightTimeGraphWidth = 550;
 var averageFlightTimeGraphHeight = 550;
 
+var batViewerWidth = 500;
+var batViewerHeight = 500;
+
 var calibrator =             new Calibrator            (deviceWidth,                 deviceHeight,calibratorScale);
 var caveDMYSelector =        new CaveDMYSelector       (caveDMYSelectorWidth,        caveDMYSelectorHeight);
 var populationGraph =        new PopulationGraph       (populationGraphWidth,        populationGraphHeight);
 var flightsHistogram =       new FlightsHistogram      (flightsHistogramWidth,       flightsHistogramHeight);
 var averageFlightTimeGraph = new AverageFlightTimeGraph(averageFlightTimeGraphWidth, averageFlightTimeGraphHeight);
+var batViewer = new BatViewer(batViewerWidth, batViewerHeight);
 
 var calibratorChangeDispatch = d3.dispatch("calibratorChanged");
 calibratorChangeDispatch.on("calibratorChanged", function() {
@@ -50,19 +54,4 @@ batListDispatch.on("batListChanged", function() {
 calibrator.dispatch      = calibratorChangeDispatch;
 populationGraph.dispatch = batListDispatch;
 
-d3.json("files/20141003_s3dr.json", function(error, data) {
-	if (error) { throw error; }
-
-	data.bats.sort(function(a, b){
-	  if(a.track[0].f < b.track[0].f)
-	    return -1;
-	  else if(a.track[0].f > b.track[0].f)
-	    return 1;
-	  else 
-	    return 0;
-	});
-
-	var scene = new Scene(data, 500, 500);
-	scene.init();
-	scene.animate();
-});		
+batViewer.loadFile("files/20141003_s3dr.json");
